613a79648af28c9dc6d3874742d4185f17b151f3
Ritu <gandhi.ritu1990@gmail.com>
2023-03-16 15:22:16 +0530

    Added more classes

--- src/test/java/com/basics/Basic.java
+++ src/test/java/com/basics/Basic.java
@@ -1,10 +1,43 @@
 package com.basics;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.given;
 
 public class Basic {
     public static void main(String[] args){
+        //given - all input details
+        // when - submit API (POST/GET/PUT/DELETE) and resource
+        //then  -validate response
         RestAssured.baseURI = "https://rahulshettyacademy.com";
+        String response = given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body("{\n"
+                        + "  \"location\": {\n"
+                        + "    \"lat\": -38.383494,\n"
+                        + "    \"lng\": 33.427362\n"
+                        + "  },\n"
+                        + "  \"accuracy\": 50,\n"
+                        + "  \"name\": \"Frontline house\",\n"
+                        + "  \"phone_number\": \"(+91) 983 893 3937\",\n"
+                        + "  \"address\": \"30, side layout, cohen 09\",\n"
+                        + "  \"types\": [\n"
+                        + "    \"shoe park\",\n"
+                        + "    \"shop\"\n"
+                        + "  ],\n"
+                        + "  \"website\": \"http://google.com\",\n"
+                        + "  \"language\": \"French-IN\"\n"
+                        + "}\n").when().post("/maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+
+        //log().all() method with given and then is used to log complete request & response
+
+
+        System.out.println("response: " +response);
+        //JSONPath is used to parse parse string as json and takes string as input
+        JsonPath js = new JsonPath(response);
+        System.out.println(js.getString("place_id"));
+
 
     }
 }
---
+++ src/test/java/com/basics/ParseComplexJSON.java
@@ -0,0 +1,19 @@
+package com.basics;
+
+import com.testData.SampleJSON;
+import io.restassured.path.json.JsonPath;
+
+public class ParseComplexJSON {
+
+    public static void main(String[] args){
+        JsonPath js = new JsonPath(SampleJSON.complexJSON());
+        //Total no. of courses
+        System.out.println("Total no. of courses: " +js.getInt("courses.size()"));
+        // purchase amount
+        System.out.println("Purchase amount: " +js.getInt("dashboard.purchaseAmount"));
+        // Title of first course
+        System.out.println("Title of first course: " +js.getString("courses[0].title"));
+
+
+    }
+}
